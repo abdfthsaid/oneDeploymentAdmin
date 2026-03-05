8af66a39238384c17156b65d1c20293808e0dbb1
abdfthsaid <abdifatahsaedmxm@gmail.com>
2026-03-05 11:36:58 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  cashComponent: true,
   experimental: {
     instrumentationHook: true,
     optimizePackageImports: [
